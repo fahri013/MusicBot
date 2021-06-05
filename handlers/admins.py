c042d66cd31c3d821bad8681dbfc194fc745e3b0
fahri013 <82764454+fahri013@users.noreply.github.com>
2021-06-05 17:36:18 +0800

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -37,10 +37,10 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text("**⭐KINGBOT⭐**:❗ Nothing is playing!")
+        await message.reply_text("**Arditobot**:❗ Tidak ada yg terputar!")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: ▶️ Paused!")
+        await message.reply_text("**Arditobot**: ▶️ Ditunda!")
 
 
 @Client.on_message(command("resume") & other_filters)
@@ -50,12 +50,12 @@ async def resume(_, message: Message):
     if (
             message.chat.id not in callsmusic.pytgcalls.active_calls
     ) or (
-            callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'memutar'
     ):
-        await message.reply_text("**⭐KINGBOT⭐**: 😜 Nothing is paused!")
+        await message.reply_text("**Arditobot**: 😜 Tdk ada yg tertunda!")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: ⏸ Resumed!!")
+        await message.reply_text("**Arditobot**: ⏸ Tunda haha!!")
 
 
 @Client.on_message(command("end") & other_filters)
@@ -63,7 +63,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("**⭐KINGBOT⭐**: 😅 Nothing is streaming!")
+        await message.reply_text("**Arditobot**: 😅 Apa mau mu stop tolo!")
     else:
         try:
             callsmusic.queues.clear(message.chat.id)
@@ -71,7 +71,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text("**⭐KINGBOT⭐**: 🙇Stopped streaming!")
+        await message.reply_text("**Arditobot**: 🙇Lagu Terhenti!")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -80,7 +80,7 @@ async def stop(_, message: Message):
 async def skip(_, message: Message):
     global que
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("**⭐KINGBOT⭐**:❗ Nothing is playing boss to skip!! I think its last song! or not playing song!!")
+        await message.reply_text("**Arditobot**:❗ Nothing is playing boss to skip!! I think its last song! or not playing song!!")
     else:
         callsmusic.queues.task_done(message.chat.id)
 
@@ -98,7 +98,7 @@ async def skip(_, message: Message):
         skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text(f"- ♂️ Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+    await message.reply_text(f"- ♂️ Skipped **{skip[0]}**\n- Yg tapputar skrng**{qeue[0][0]}**")
 
 
 @Client.on_message(
@@ -107,4 +107,4 @@ async def skip(_, message: Message):
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text("**⭐KINGBOT⭐**: Admin cache refreshed!")
+    #await message.reply_text("**Arditobot**: Admin cache refreshed!")
